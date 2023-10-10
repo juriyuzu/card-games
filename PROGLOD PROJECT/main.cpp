8b2305e2596e3c4297498b894fefbb7ccf6788f8
juriyuzu <98145551+juriyuzu@users.noreply.github.com>
2023-10-10 21:11:07 +0800

    Add files via upload

---
+++ PROGLOD PROJECT/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <ctime>
+using namespace std;
+
+struct cards {
+	string cards[52];
+	
+};
+
+void poker() {
+	
+}
+
+int main() {
+	poker();
+	
+	return 0;
+}
