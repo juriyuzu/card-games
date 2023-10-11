e35e5291d4ace07069c3821c7e008a5e2da05a13
juriyuzu <juliusantonaquino.joules@gmail.com>
2023-10-11 08:49:56 +0800

    upd

--- PROGLOD PROJECT/main.cpp
+++ PROGLOD PROJECT/main.cpp
@@ -7,6 +7,10 @@ using namespace std;
 string suits[4] = {"C", "S", "H", "D"};
 string values[13] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
 
+int rng(int min, int max) {
+	return min + (rand() % (max - min));
+}
+
 struct Cards {
     string cards[52] = {"AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD"};
 
@@ -16,14 +20,20 @@ struct Cards {
         }
         cout << endl;
     }
+    
+    void shuffleCards() {
+    	shuffle(cards, cards + sizeof(cards), rand());
+	}
 };
 
 void poker() {
 	Cards cards;
+	cards.shuffleCards();
     cards.printCards();
 }
 
 int main() {
+	srand(time(0));
 	poker();
 	
 	return 0;
